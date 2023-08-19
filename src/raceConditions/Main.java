dd52789c15aa26a95b4ef3810e3762dea495a68f
nic <mohsenmn2079@gmail.com>
2023-08-19 11:09:25 +0330

    fixed buged and the result

--- src/raceConditions/Main.java
+++ src/raceConditions/Main.java
@@ -13,10 +13,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 class Account {
-    int id;
-    String name;
-    long balance;
-    private final Object lock = new Object(); // Lock for synchronization
+    private int id;
+    private String name;
+    private Long balance;
 
     public Account(int id, String name, long balance) {
         this.id = id;
@@ -24,21 +23,29 @@ class Account {
         this.balance = balance;
     }
 
-    public void withdraw(long amount) {
-        synchronized (lock) {
-            balance -= amount;
-        }
+    public int getId() {
+        return id;
     }
 
-    public void deposit(long amount) {
-        synchronized (lock) {
-            balance += amount;
-        }
+    public String getName() {
+        return name;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public synchronized void withdraw(long amount) {
+        balance -= amount;
+    }
+
+    public synchronized void deposit(long amount) {
+        balance += amount;
     }
 }
 
-record Transaction(int fromAccount, int toAccount, long amount) {}
-
+record Transaction(int fromAccount, int toAccount, long amount) {
+}
 
 
 class FraudDetection implements Runnable {
@@ -58,7 +65,6 @@ class FraudDetection implements Runnable {
             if (fromAccount != null && toAccount != null) {
                 fromAccount.withdraw(transaction.amount());
                 toAccount.deposit(transaction.amount());
-                System.out.println(fromAccount.name + " " + fromAccount.balance);
             }
         }
     }
@@ -67,15 +73,19 @@ class FraudDetection implements Runnable {
 
 public class Main {
     private static final String ACCOUNT_FILE_PATH = "/Users/nic/IdeaProjects/market/Stream/src/raceConditions/account.csv";
-    private static final String TRANSACTION_FILE_PATH = "/Users/nic/IdeaProjects/market/Stream/src/raceConditions/account.csv";
+    private static final String TRANSACTION_FILE_PATH = "/Users/nic/IdeaProjects/market/Stream/src/raceConditions/transaction.csv";
+
     public static void main(String[] args) {
         Map<Integer, Account> accounts = readAccountsFromFile(ACCOUNT_FILE_PATH);
+        Map<Integer, Account> primaryAccounts = readAccountsFromFile(ACCOUNT_FILE_PATH);
         List<Transaction> transactions = readTransactionsFromFile(TRANSACTION_FILE_PATH);
 
         int numThreads = Runtime.getRuntime().availableProcessors();
         ExecutorService executor = Executors.newFixedThreadPool(numThreads);
 
         int batchSize = (int) Math.ceil((double) transactions.size() / numThreads);
+
+
         List<Callable<Void>> tasks = new ArrayList<>();
 
         for (int i = 0; i < numThreads; i++) {
@@ -87,17 +97,22 @@ public class Main {
                 new FraudDetection(accounts, batch).run();
                 return null;
             };
-
             tasks.add(task);
         }
 
         try {
             executor.invokeAll(tasks);
         } catch (InterruptedException e) {
-            e.printStackTrace(); // Handle or log the exception as needed
+            e.printStackTrace();
         } finally {
             executor.shutdown();
         }
+
+        System.out.println("primary account:");
+        primaryAccounts.forEach((integer, account) -> System.out.println("id: " + integer + " balance: " + account.getBalance()));
+        System.out.println();
+        System.out.println("real account");
+        accounts.forEach((integer, account) -> System.out.println("id: " + integer + " balance: " + account.getBalance()));
 
     }
 
@@ -110,8 +125,9 @@ public class Main {
         int value = array[index];
         System.out.println("Reading " + value + " from index " + index);
     }
-    private static Map<Integer,raceConditions.Account> readAccountsFromFile(String filename) {
-        Map<Integer,raceConditions.Account> accounts = new HashMap<>();
+
+    private static Map<Integer, raceConditions.Account> readAccountsFromFile(String filename) {
+        Map<Integer, raceConditions.Account> accounts = new HashMap<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             boolean header = false;
@@ -121,12 +137,12 @@ public class Main {
                     continue;
                 }
 
-                String[] parts = line.split(",",3);
+                String[] parts = line.split(",", 3);
                 if (parts.length == 3) {
                     int id = Integer.parseInt(parts[0]);
                     String name = parts[1];
                     long balance = parseBalance(parts[2]);
-                    accounts.put(id,new raceConditions.Account(id, name, balance));
+                    accounts.put(id, new raceConditions.Account(id, name, balance));
                 }
             }
         } catch (IOException e) {
@@ -137,7 +153,7 @@ public class Main {
     }
 
     private static long parseBalance(String balanceStr) {
-        return Long.parseLong(balanceStr.replace(",", "").replace("\"",""));
+        return Long.parseLong(balanceStr.replace(",", "").replace("\"", ""));
     }
 
     private static List<raceConditions.Transaction> readTransactionsFromFile(String filename) {
@@ -151,10 +167,10 @@ public class Main {
                     continue;
                 }
 
-                String[] parts = line.split(",",3);
+                String[] parts = line.split(",", 3);
                 if (parts.length == 3) {
                     int fromAccount = Integer.parseInt(parts[0]);
-                    int toAccount = Integer.parseInt(parts[0]);
+                    int toAccount = Integer.parseInt(parts[1]);
                     long amount = parseBalance(parts[2]);
 
                     transactions.add(new raceConditions.Transaction(fromAccount, toAccount, amount));
